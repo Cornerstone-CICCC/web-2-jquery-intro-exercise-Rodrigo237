764c45bd6d8422589bc1d3d10be81bf57de9edb6
Rodrigo237 <36214941+Rodrigo237@users.noreply.github.com>
2025-08-06 15:45:09 -0700

    Exercise Done

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 $(function() {
   // YOUR CODE HERE
+  $('#toggleButton').on('click', function() {
+    $('#myParagraph').slideToggle();
+  })
+
+
+  $('#colorButton').on('click',function(){
+    $('#colorDiv').css('background-color','red');
+  })
+
+  $('#addClassButton').on('click', function() {
+  $('#classDiv').addClass('newClass');
+  })
+
+  $('#removeClassButton').on('click', function() {
+  $('#classDiv').removeClass('newClass');
+  })
+
+  $('#fadeInButton').on('click',function(){
+    $('#fadeDiv').fadeIn()
+  })
+
+  $('#fadeOutButton').on('click',function(){
+    $('#fadeDiv').fadeOut()
+  })
+
+  $('#slideUpButton').on('click',function(){
+    $('#slideDiv').slideUp()
+  })
+
+  $('#slideDownButton').on('click',function(){
+    $('#slideDiv').slideDown()
+  })
 })
